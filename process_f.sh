51ba96c48dac4e3ffc073ae4be2f60a6e8a78e99
Leonardo Costa <leonardocosta@ua.pt>
2020-11-04 19:10:44 +0000

    added script to see execution interval

---
+++ process_f.sh
@@ -0,0 +1,21 @@
+#!/bin/env sh
+
+b=$(cat log.txt | grep -e "ta (s:ns)" | cut -d" " -f4- | awk '{split($0,a,":"); print a[2]}');
+
+min=10000000001000
+max=0
+for i in ${b}
+do
+    if [ "$i" -lt "$min" ]
+    then
+        min=$i
+        echo min=$min max=$max
+    fi
+    if [ "$i" -gt "$max" ]
+    then
+        max=$i
+        echo min=$min max=$max
+    fi
+done
+
+
